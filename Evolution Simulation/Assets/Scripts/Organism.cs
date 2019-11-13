b73000831921e45afdcefc9151453d584477fd86
Jacob Carr <jcarr343@hotmail.co.uk>
2019-11-13 16:22:40 +0000

    Creatures initialised

---
+++ Evolution Simulation/Assets/Scripts/Organism.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Organism : MonoBehaviour
+{
+    [SerializeField]
+    private float health;
+    [SerializeField]
+    private float energy;
+    [SerializeField]
+    private float detectionRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = Random.Range(50f, 150f);
+        energy = Random.Range(50f, 150f);
+        detectionRadius = Random.Range(5f, 15f);
+    }
+
+    void Update()
+    {
+        Vector3 translate = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+        translate *= Time.deltaTime;
+        transform.Translate(translate);
+        energy -= Time.deltaTime;
+        if (health < 100)
+        {
+            health += Time.deltaTime / 2;
+            energy -= Time.deltaTime / 2;
+        }
+        if (energy <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Evolution Simulation/Assets/Scripts/SpawnCreatures.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCreatures : MonoBehaviour
+{
+    public GameObject creature;
+    public int spawn;
+    public Vector2 range;
+
+    void Start()
+    {
+        for (int i = 0; i < spawn; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-range.x, range.x), 0.5f, Random.Range(-range.y, range.y));
+            Instantiate(creature, pos, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
